73791fb18687e18c12c26f47318f11495cc19efc
naafiyan <naafiyan@gmail.com>
2021-01-08 01:17:23 +0600

    changed api entry in cra

--- client/src/config/urls.ts
+++ client/src/config/urls.ts
@@ -1,4 +1,4 @@
-//const API_URL = "https://post-it-naafiyan.herokuapp.com"; // for prod
-const API_URL = "http://localhost:5000/api"; // for local
+const API_URL = "https://post-it-naafiyan.herokuapp.com/api"; // for prod
+//const API_URL = "http://localhost:5000/api"; // for local
 
 export default API_URL;
